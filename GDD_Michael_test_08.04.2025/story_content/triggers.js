6c16dc1e330160404060bee4457342376cbaee64
mollywells <molly.wells@mail.utoronto.ca>
2025-04-08 22:31:33 -0400

    new repository!

---
+++ GDD_Michael_test_08.04.2025/story_content/triggers.js
@@ -0,0 +1,42 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6VRVEX10jBz":
+        Script1();
+        break;
+      case "6iY0wiZIHVs":
+        Script2();
+        break;
+      case "5caE6SAJrRV":
+        Script3();
+        break;
+      case "6VkLlQlZljV":
+        Script4();
+        break;
+      case "6cjdfyOa42g":
+        Script5();
+        break;
+      case "6oSdefXXLWF":
+        Script6();
+        break;
+      case "60Q7918MmPB":
+        Script7();
+        break;
+      case "62nNuKiQtDl":
+        Script8();
+        break;
+      case "5W6PbL24TQt":
+        Script9();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
